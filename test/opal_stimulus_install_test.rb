71377d441afc29bd6839a5e70cbce4c5b5997973
Joseph Schito <joseph.schito@gmail.com>
2025-08-18 11:57:55 +0200

    Test installation on Rails

---
+++ test/opal_stimulus_install_test.rb
@@ -0,0 +1,83 @@
+# test/opal_stimulus_install_test.rb
+require "minitest/autorun"
+require "fileutils"
+require "open3"
+require "tmpdir"
+
+class OpalStimulusInstallTest < Minitest::Test
+  parallelize_me!
+
+  def setup
+    @tmpdir = Dir.mktmpdir
+    @app_path = File.join(@tmpdir, "dummy_app")
+  end
+
+  def teardown
+    FileUtils.remove_entry @tmpdir
+  end
+
+  def run_command(cmd, chdir: @tmpdir)
+    out, err, status = Open3.capture3(cmd, chdir: chdir)
+    unless status.success?
+      flunk "Command failed: #{cmd}\nSTDOUT:\n#{out}\nSTDERR:\n#{err}"
+    end
+    out
+  end
+
+  def test_install_opal_stimulus_in_rails_7_2
+    test_on_rails("7.2")
+  end
+
+  def test_install_opal_stimulus_in_rails_8
+    test_on_rails("8")
+  end
+
+  private
+
+  def test_on_rails(version)
+    run_command("gem install rails -v #{version}")
+    run_command("rails _#{version}_ new dummy_app", chdir: @tmpdir)
+
+    gemfile_path = File.join(@app_path, "Gemfile")
+    gemfile = File.read(gemfile_path)
+    gemfile << "\ngem 'bootsnap', require: false\n" unless gemfile.include?("bootsnap")
+    gemfile << "\ngem 'opal_stimulus', path: '../../'\n"
+    File.write(gemfile_path, gemfile)
+
+    boot_path = File.join(@app_path, "config/boot.rb")
+    boot = File.read(boot_path)
+    boot = boot.gsub(/require ["']bootsnap\/setup["']/, "# require 'bootsnap/setup'")
+    File.write(boot_path, boot)
+
+    FileUtils.rm_f(File.join(@app_path, "config", "initializers", "assets.rb"))
+
+    run_command("bundle install", chdir: @app_path)
+
+    out, err, status = Open3.capture3("bin/rails opal_stimulus:install", chdir: @app_path)
+    unless status.success?
+      flunk "opal_stimulus:install failed\nSTDOUT:\n#{out}\nSTDERR:\n#{err}"
+    end
+
+    ["my", "my/best"].each do |gen|
+      Open3.capture3("bin/rails g opal_stimulus #{gen}", chdir: @app_path)
+    end
+    Open3.capture3("bin/rails g controller pages index", chdir: @app_path)
+
+    procfile_path = File.join(@app_path, "Procfile.dev")
+    assert File.exist?(procfile_path)
+    assert File.read(procfile_path).include?("opal: bin/rails opal_stimulus:watch")
+    assert File.exist?(File.join(@app_path, "app/assets/builds/.keep"))
+    assert File.exist?(File.join(@app_path, "app/opal/controllers/.keep"))
+    assert File.exist?(File.join(@app_path, "app/opal/controllers/my_controller.rb"))
+    assert File.exist?(File.join(@app_path, "app/opal/controllers/my/best_controller.rb"))
+    assert File.exist?(File.join(@app_path, "app/opal/application.rb"))
+    assert File.exist?(File.join(@app_path, "app/views/layouts/application.html.erb"))
+    assert File.exist?(File.join(@app_path, "bin/dev"))
+    gitignore = File.join(@app_path, ".gitignore")
+    assert File.exist?(gitignore)
+    content = File.read(gitignore)
+    assert content.include?("/.opal-cache")
+    assert content.include?("app/assets/builds/opal.js")
+    assert content.include?("app/assets/builds/opal.js.map")
+  end
+end
